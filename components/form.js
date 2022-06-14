5951bfa25eb2ffba778c7f6bd9f274b646468017
ndi-dev90 <ndi.dev90@gmail.com>
2022-06-14 21:59:59 +0200

    commit on 'main'

--- components/form.js
+++ components/form.js
@@ -17,16 +17,12 @@ export default {
         }
     },
     template: `
-    <style>
-        label {
-            color: red;
-        }
-    </style>
-    <div class="formdiv">
-      <label>name</label>
-      <input :value="text" @input="event => form.name = event.target.value">
-      <label>age</label>
-      <input :value="text" @input="event => form.age = event.target.value">
-      <button @click="event => this.$emit('submitted',form)">submit</button>
-    </div>`
+        <div class="formdiv">
+            <label>name</label>
+            <input :value="text" @input="event => form.name = event.target.value">
+            <label>age</label>
+            <input :value="text" @input="event => form.age = event.target.value">
+            <button class="submitFrm" @click="event => this.$emit('submitted',form)">submit</button>
+        </div>
+    `
 }
