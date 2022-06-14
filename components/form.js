1081932910c8f2994bb458081f3c5ae612e61b32
ndi-dev90 <ndi.dev90@gmail.com>
2022-06-14 17:47:41 +0200

    main

---
+++ components/form.js
@@ -0,0 +1,27 @@
+export default {
+    name: 'formcomp',
+    props: ['id'],
+    data() {
+        return {
+            form: {
+                id: this.id,
+                name: '',
+                age: null
+            }
+        }
+    },
+    methods: {
+        submForm() {
+            alert('csé')
+            this.$emit('submitted', form)
+        }
+    },
+    template: `
+    <div class="formdiv">
+      <label>name</label>
+      <input :value="text" @input="event => form.name = event.target.value">
+      <label>age</label>
+      <input :value="text" @input="event => form.age = event.target.value">
+      <button @click="event => this.$emit('submitted',form)">submit</button>
+    </div>`
+}
